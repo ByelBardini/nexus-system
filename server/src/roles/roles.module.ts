4622c8a74e32a25a6c5094ea6088447bf97b9d44
ByelBardini <gabryel.bgorleans@gmail.com>
2026-04-21 22:08:22 -0300

    fix(roles): importar UsersModule para injeção do UsersService
    O PermissionsGuard depende de UsersService; o RolesModule importa apenas
    PrismaModule, o que quebrava a resolução de dependências. Importa
    UsersModule e documenta o motivo no comentário do @Module.

--- server/src/roles/roles.module.ts
+++ server/src/roles/roles.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { RolesController } from './roles.controller';
 import { RolesService } from './roles.service';
 import { PrismaModule } from '../prisma/prisma.module';
+import { UsersModule } from '../users/users.module';
 
+/** `PermissionsGuard` injeta `UsersService`; o módulo de usuários deve ser importado aqui. */
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, UsersModule],
   controllers: [RolesController],
   providers: [RolesService],
   exports: [RolesService],
